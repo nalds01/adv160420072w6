0d1ee78ac6211c34bfa5f86ed562c658a28de7e0
nalds01 <s160420072@student.ubaya.ac.id>
2023-03-20 13:06:21 +0700

    init

---
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420072week4/StudentDetailFragment.kt
@@ -0,0 +1,21 @@
+package id.ac.ubaya.informatika.adv160420072week4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class StudentDetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+}
